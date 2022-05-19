cad0660a59f3a34d696c8cb4f2dcfa2cf5d0806b
Rei-x <wszyzdgo@gmail.com>
2022-05-19 22:53:26 +0000

    Add logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,32 @@
 import { auth } from '@/firebase';
-import { Container, HStack, Heading, Box, Link } from '@chakra-ui/react';
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import { Container, HStack, Heading, Box, Link, Button, Menu, MenuButton, MenuItem, MenuList, Spinner } from '@chakra-ui/react';
+import { useAuthState } from "react-firebase-hooks/auth";
 import NextLink from "next/link";
 import React from 'react';
+import { signOut } from 'firebase/auth';
 
 const Navbar = () => {
-
+  const [user, loading] = useAuthState(auth);
 
   return (
     <Container maxW="container.lg">
       <HStack mx="auto" maxWidth={"70rem"} mt="6" justifyContent="space-between">
         <NextLink href="/"><Heading cursor="pointer" as="h1">Gutenberg books</Heading></NextLink>
         <Box>
-          {auth.currentUser ? <NextLink href={`/user/${auth.currentUser.uid}`}>
-            <Link>{auth.currentUser.displayName}</Link>
-          </NextLink> : <NextLink href="/login">
+          {loading && <Spinner />}
+          {!loading && user && <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              {user.displayName || "Anonymous"}
+            </MenuButton>
+            <MenuList>
+              <NextLink href="/favorite-books"><MenuItem>Favorite books</MenuItem></NextLink>
+              <MenuItem onClick={() => signOut(auth)}>Logout</MenuItem>
+            </MenuList>
+          </Menu>}
+          {!loading && !user && <NextLink href="/login">
             <Link>Login</Link>
           </NextLink>}
-
         </Box>
       </HStack>
     </Container>
