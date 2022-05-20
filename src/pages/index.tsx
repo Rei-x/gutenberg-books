c05be306e899831d8c7e2b38b1905f39978a0398
Rei-x <wszyzdgo@gmail.com>
2022-05-20 16:29:41 +0000

    Delete unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,9 @@
-import { Box, Button, Container, Heading, HStack, Input, Link, VStack, Text, Select, Spinner, Image, Wrap } from '@chakra-ui/react';
+import { Box, Container } from '@chakra-ui/react';
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import NextLink from "next/link";
-import { useMemo, useState } from 'react';
-import { useInfiniteQuery, useQuery } from 'react-query';
-import { useDebounce } from "use-debounce";
+import React, { useState } from 'react';
 import BookList from '../components/BookList';
-import BookView from '../components/BookView';
 import Filters from '../components/Filters';
 import Layout from '../components/Layout';
-import Navbar from '../components/Navbar';
 import SearchInput from '../components/SearchInput';
 import useBooks from '@/hooks/useBooks';
 
