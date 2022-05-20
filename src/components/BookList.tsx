80498c19d590770ff049b5b2d143410bc458e586
Rei-x <wszyzdgo@gmail.com>
2022-05-20 16:38:16 +0000

    Add infinity scroll

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -4,26 +4,23 @@ import { UseInfiniteQueryResult } from 'react-query';
 import { ListBooks } from '@/types/booksGet';
 import BookView from './BookView';
 import { AnimatePresence, motion } from 'framer-motion';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Loading from './Loading';
 
 const BookList = ({ booksQuery }: { booksQuery: UseInfiniteQueryResult<ListBooks, unknown>; }) => {
+
+  const booksLength = booksQuery.data?.pages.reduce((acc, cur) => {
+    return acc + cur.results.length;
+  }, 0);
+
   return (
     <VStack as={AnimatePresence}>
-      {booksQuery.isLoading && <VStack layout as={motion.div} initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}>
-        <Spinner />
-        <Text>It can take up to a minute to load the data..</Text>
-      </VStack>}
+      {booksQuery.isLoading && <Loading />}
       {booksQuery.isSuccess && <VStack gap="8" >
-        {booksQuery.data?.pages.map((page) => page.results.map((book) =>
-          <BookView key={book.id} book={book} />))}
-        {booksQuery.hasNextPage &&
-          <Button
-            onClick={() => booksQuery.fetchNextPage()}
-            isLoading={booksQuery.isFetchingNextPage}
-          >
-            Load more
-          </Button>}
+        <InfiniteScroll style={{ overflow: "hidden" }} hasMore={booksQuery.hasNextPage || false} dataLength={booksLength || 0} next={booksQuery.fetchNextPage} loader={<Loading />}>
+          {booksQuery.data?.pages.map((page) => page.results.map((book) =>
+            <BookView key={book.id} book={book} />))}
+        </InfiniteScroll>
         {booksQuery.data?.pages[0].count === 0 && <Text layout as={motion.div} initial={{ opacity: 0, position: "absolute" }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0, position: "absolute" }}>There were no books that matched your query</Text>}
---
+++ src/components/Loading.tsx
@@ -0,0 +1,14 @@
+import { VStack, Spinner, Text } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+import React from 'react'
+
+const Loading = () => 
+    <VStack layout as={motion.div} initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}>
+      <Spinner />
+      <Text>It can take up to a minute to load the data..</Text>
+    </VStack>
+
+
+export default Loading
