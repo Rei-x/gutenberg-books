e71f62f972ec3c25e30fc0095c21eec3f2707dd9
Rei-x <wszyzdgo@gmail.com>
2022-06-03 09:37:20 +0000

    Fix Image link not redirecting to book

--- src/components/BookView.tsx
+++ src/components/BookView.tsx
@@ -1,41 +1,60 @@
-import { HStack, Box, Heading, Link, Image, Text, Skeleton } from '@chakra-ui/react';
+import {
+  HStack,
+  Box,
+  Heading,
+  Link,
+  Image,
+  Text,
+  Skeleton,
+} from "@chakra-ui/react";
 import NextLink from "next/link";
-import React, { useMemo } from 'react';
-import { Book, ResourceType } from '../types/booksGet';
-import MarkAsFavorite from './MarkAsFavorite';
-import { motion } from 'framer-motion';
-import { truncateString } from '../utils/truncateString';
-import ImageWithSkeleton from './ImageWithSkeleton';
+import React, { useMemo } from "react";
+import { Book, ResourceType } from "../types/booksGet";
+import MarkAsFavorite from "./MarkAsFavorite";
+import { motion } from "framer-motion";
+import { truncateString } from "../utils/truncateString";
+import ImageWithSkeleton from "./ImageWithSkeleton";
 
 const getImageFromBook = (book: Book) => {
-  return book.resources.find((resource) => resource.type === ResourceType.ImageJPEG && resource.uri.endsWith("medium.jpg"))?.uri;
+  return book.resources.find(
+    (resource) =>
+      resource.type === ResourceType.ImageJPEG &&
+      resource.uri.endsWith("medium.jpg")
+  )?.uri;
 };
 
 const getAuthorFromBook = (book: Book) => {
   return book.agents.find((agent) => agent.type === "Author")?.person;
 };
 
-const BookView = ({ book }: { book: Book; }) => {
+const BookView = ({ book }: { book: Book }) => {
   const image = useMemo(() => getImageFromBook(book), [book]);
 
   return (
-    <HStack
-      width={["sm", "md"]}
-      alignItems="flex-start" key={book.id} >
+    <HStack width={["sm", "md"]} alignItems="flex-start" key={book.id}>
       <Box width="30%">
-        {image && <Link>
+        {image && (
           <NextLink href={`/book/${book.id}`}>
-            <ImageWithSkeleton src={image} width="100%" height="13rem" alt="Cover of the book" />
+            <Link>
+              <ImageWithSkeleton
+                src={image}
+                width="100%"
+                height="13rem"
+                alt="Cover of the book"
+              />
+            </Link>
           </NextLink>
-        </Link>}
+        )}
       </Box>
       <Box width="70%">
         <div>
-          <Link>
-            <NextLink href={`/book/${book.id}`}>
-              <Heading data-testid="title" as="h2" size="md">{truncateString(book.title)}</Heading>
-            </NextLink>
-          </Link>
+          <NextLink href={`/book/${book.id}`}>
+            <Link>
+              <Heading data-testid="title" as="h2" size="md">
+                {truncateString(book.title)}
+              </Heading>
+            </Link>
+          </NextLink>
           <Text data-testid="downloads">Downloads: {book.downloads}</Text>
           <Text data-testid="author">Author: {getAuthorFromBook(book)}</Text>
         </div>
--- src/components/ImageWithSkeleton.tsx
+++ src/components/ImageWithSkeleton.tsx
@@ -1,12 +1,34 @@
-import { Skeleton, Image } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import { Skeleton, Image } from "@chakra-ui/react";
+import React, { useState } from "react";
 
-const ImageWithSkeleton = ({ src, alt, height, width }: { src: string, alt: string, height: string, width: string; }) => {
+const ImageWithSkeleton = ({
+  src,
+  alt,
+  height,
+  width,
+}: {
+  src: string;
+  alt: string;
+  height: string;
+  width: string;
+}) => {
   const [isLoaded, setIsLoaded] = useState(false);
 
   return (
-    <Skeleton fadeDuration={2} speed={5} height={height} width={width} isLoaded={isLoaded}>
-      <Image onLoad={() => setIsLoaded(true)} height={height} width={width} src={src} alt={alt} />
+    <Skeleton
+      fadeDuration={2}
+      speed={5}
+      height={height}
+      width={width}
+      isLoaded={isLoaded}
+    >
+      <Image
+        onLoad={() => setIsLoaded(true)}
+        height={height}
+        width={width}
+        src={src}
+        alt={alt}
+      />
     </Skeleton>
   );
 };
